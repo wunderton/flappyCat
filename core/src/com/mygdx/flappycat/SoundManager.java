8d4d7bde1ba156e3d80b5c46ea63e14b85e41fa0
Antonio Carrasco <wunderton@gmail.com>
2020-01-25 16:58:27 +0100

    Añadido sonido de gato al caer y horientacion vertical para moviles

--- core/src/com/mygdx/flappycat/SoundManager.java
+++ core/src/com/mygdx/flappycat/SoundManager.java
@@ -7,7 +7,7 @@ public class SoundManager {
 
     public static Sound WOOSH = Gdx.audio.newSound(Gdx.files.internal("woosh.ogg"));
     public static Sound FLAP = Gdx.audio.newSound(Gdx.files.internal("flap.ogg"));
-    public static Sound HIT = Gdx.audio.newSound(Gdx.files.internal("hit.ogg"));
+    public static Sound HIT = Gdx.audio.newSound(Gdx.files.internal("hit.mp3"));
     public static Sound FALL = Gdx.audio.newSound(Gdx.files.internal("fall.ogg"));
     public static Sound POINT = Gdx.audio.newSound(Gdx.files.internal("point.ogg"));
 
