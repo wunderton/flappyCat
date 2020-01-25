88b1cacb3c5f42de6d3e94a850712a454f753ee9
Antonio Carrasco <wunderton@gmail.com>
2020-01-25 20:27:20 +0100

    Cambiado sprite del gato por mi gato naranjito

--- core/src/com/mygdx/flappycat/TextureManager.java
+++ core/src/com/mygdx/flappycat/TextureManager.java
@@ -9,7 +9,7 @@ public class TextureManager {
     public static Texture MENU =        new Texture(Gdx.files.internal("logoCat.png"));
     public static Texture GET_READY =   new Texture(Gdx.files.internal("ready.png"));
     public static Texture BG_GROUND =   new Texture(Gdx.files.internal("bg_ground.png"));
-    public static Texture CAT =         new Texture(Gdx.files.internal("cat.png"));
+    public static Texture CAT =         new Texture(Gdx.files.internal("naranjito.png"));
     public static Texture PIPE_UP =     new Texture(Gdx.files.internal("pipe_up.png"));
     public static Texture PIPE_DOWN =   new Texture(Gdx.files.internal("pipe_down.png"));
     public static Texture GAME_OVER =   new Texture(Gdx.files.internal("game_over.png"));
